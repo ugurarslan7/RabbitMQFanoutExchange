55bed684eb81362ccb5381ea19270a8535d09290
ugurarslan7 <ugurarslan8@hotmail.com>
2024-09-13 23:44:19 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqps://jcabnccy:j5e8IUxSyGYiI93gEH0wYlJhfsuPNbQk@stingray.rmq.cloudamqp.com/jcabnccy");
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+        Enumerable.Range(0, 50).ToList().ForEach(Range =>
+        {
+            string message = Range.ToString() + " .Message";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+            Console.WriteLine(message);
+        });
+
+        Console.ReadLine();
+
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqps://jcabnccy:j5e8IUxSyGYiI93gEH0wYlJhfsuPNbQk@stingray.rmq.cloudamqp.com/jcabnccy");
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        var randomQueneName = channel.QueueDeclare().QueueName;
+
+        channel.QueueBind(randomQueneName, "logs-fanout", "", null);
+
+        channel.BasicQos(0, 1, false);
+        
+        var subscriber = new EventingBasicConsumer(channel);
+
+        channel.BasicConsume(randomQueneName, false, subscriber);
+
+        Console.WriteLine("Logs are listening...");
+
+        subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            Thread.Sleep(1000);
+
+            //Clear incoming message
+            channel.BasicAck(e.DeliveryTag, false);
+
+            Console.WriteLine(message);
+
+        };
+
+        Console.ReadLine();
+
+    }
+
+}
